0a4b087cad298ba2cfa7482e41ae31e5dc615105
HBuczynski <hubert.buczynski94@gmail.com>
2017-12-29 12:19:46 +0100

    Added config file.

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo --- BOOST installation ---
+sudo apt-get install libboost1.62-all-dev 
+
+echo --- DOXYGEN installation ---
+sudo apt-get install doxygen
+
+echo --- VALGRIND installation ---
+sudo apt-get install valgrind
