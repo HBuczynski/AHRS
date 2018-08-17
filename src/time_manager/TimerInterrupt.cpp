dfc55874f62c2bea89abe6ca986636ea8159a08a
HBuczynski <hubert.buczynski94@gmail.com>
2018-08-17 13:39:07 +0200

    timer_manager: Finished timer interrupt.

--- src/time_manager/TimerInterrupt.cpp
+++ src/time_manager/TimerInterrupt.cpp
@@ -1,4 +1,13 @@
 #include <cstdint>
+#include <errno.h>
+#include <cstdio>
+#include <cstdlib>
+#include <sys/time.h>
+#include <string>
+#include <cstring>
+#include <csignal>
+#include <bits/siginfo.h>
+
 #include "TimerInterrupt.h"
 #include "TimerInterruptNotification.h"
 
@@ -6,6 +15,7 @@ using namespace std;
 using namespace utility;
 
 TimerInterrupt::TimerInterrupt()
+    : logger_(Logger::getInstance())
 {}
 
 TimerInterrupt::~TimerInterrupt()
@@ -13,12 +23,132 @@ TimerInterrupt::~TimerInterrupt()
     stop();
 }
 
-void TimerInterrupt::start(uint32_t periodInMilliseconds, TimerInterruptNotification *objectToNotify)
+void TimerInterrupt::startPeriodic(uint32_t periodInMilliseconds, TimerInterruptNotification *objectToNotify)
 {
+    struct sigevent signalEvent;
+    struct sigaction signalAction;
+    struct itimerspec timerSpecs;
 
+    uint16_t seconds = periodInMilliseconds / 1000;
+    uint32_t  nanoseconds = periodInMilliseconds*1000 - seconds*1000000;
+
+    timerSpecs.it_value.tv_sec = seconds;
+    timerSpecs.it_value.tv_nsec = nanoseconds;
+    timerSpecs.it_interval.tv_sec = seconds;
+    timerSpecs.it_interval.tv_nsec = nanoseconds;
+
+    sigemptyset(&signalAction.sa_mask);
+
+    signalAction.sa_flags = SA_SIGINFO;
+    signalAction.sa_sigaction = TimerInterrupt::handleInterrupt;
+
+    memset(&signalEvent, 0, sizeof(signalEvent));
+    signalEvent.sigev_notify = SIGEV_SIGNAL;
+    signalEvent.sigev_value.sival_ptr = (void*) objectToNotify;
+    signalEvent.sigev_signo = SIGALRM;
+
+    if (timer_create(CLOCK_REALTIME, &signalEvent, &timerID)!= 0)
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not create debounce timer");
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
+
+    if (sigaction(SIGALRM, &signalAction, NULL))
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not install new signal.");
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
+
+    if (timer_settime(timerID, 0, &timerSpecs, NULL) == -1)
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not start timer.");
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
+}
+
+void TimerInterrupt::startSingle(uint32_t timeInMilliseconds, TimerInterruptNotification* objectToNotify)
+{
+    struct sigevent signalEvent;
+    struct sigaction signalAction;
+    struct itimerspec timerSpecs;
+
+    uint16_t seconds = timeInMilliseconds / 1000;
+    uint32_t  nanoseconds = timeInMilliseconds*1000 - seconds*1000000;
+
+    timerSpecs.it_value.tv_sec = seconds;
+    timerSpecs.it_value.tv_nsec = nanoseconds;
+    timerSpecs.it_interval.tv_sec = 0;
+    timerSpecs.it_interval.tv_nsec = 0;
+
+    sigemptyset(&signalAction.sa_mask);
+
+    signalAction.sa_flags = SA_SIGINFO;
+    signalAction.sa_sigaction = TimerInterrupt::handleInterrupt;
+
+    memset(&signalEvent, 0, sizeof(signalEvent));
+    signalEvent.sigev_notify = SIGEV_SIGNAL;
+    signalEvent.sigev_value.sival_ptr = (void*) objectToNotify;
+    signalEvent.sigev_signo = SIGALRM;
+
+    if (timer_create(CLOCK_REALTIME, &signalEvent, &timerID)!= 0)
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not create debounce timer");
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
+
+    if (sigaction(SIGALRM, &signalAction, NULL))
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not install new signal.");
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
+
+    if (timer_settime(timerID, 0, &timerSpecs, NULL) == -1)
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not start timer.");
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
 }
 
 void TimerInterrupt::stop()
 {
+    if(timer_delete(timerID) == 0)
+    {
+        if(logger_.isInformationEnable())
+        {
+            const string message = string("TimerInterrupt :: Stopped timer correctly.");
+            logger_.writeLog(LogType::INFORMATION_LOG, message);
+        }
+    }
+    else
+    {
+        if(logger_.isErrorEnable())
+        {
+            const string message = string("TimerInterrupt :: Could not stop timer. Errno: ") + strerror(errno);
+            logger_.writeLog(LogType::ERROR_LOG, message);
+        }
+    }
+}
 
+void TimerInterrupt::handleInterrupt(int sigNumb, siginfo_t *si, void *uc)
+{
+    TimerInterruptNotification* notification = reinterpret_cast<TimerInterruptNotification*>(si->si_value.sival_ptr);
+    notification->interruptNotification();
 }
--- src/time_manager/TimerInterrupt.h
+++ src/time_manager/TimerInterrupt.h
@@ -1,6 +1,7 @@
 #ifndef AHRS_TIMERINTERRUPT_H
 #define AHRS_TIMERINTERRUPT_H
 
+#include <logger/Logger.h>
 #include "TimerInterruptNotification.h"
 
 namespace utility
@@ -11,10 +12,16 @@ namespace utility
         TimerInterrupt();
         ~TimerInterrupt();
 
-        void start(uint32_t periodInMilliseconds, TimerInterruptNotification* objectToNotify);
+        void startPeriodic(uint32_t periodInMilliseconds, TimerInterruptNotification* objectToNotify);
+        void startSingle(uint32_t timeInMilliseconds, TimerInterruptNotification* objectToNotify);
         void stop();
 
     private:
+        static void handleInterrupt(int sigNumb, siginfo_t *si, void *uc);
+
+        timer_t timerID;
+
+        utility::Logger& logger_;
     };
 }
 
