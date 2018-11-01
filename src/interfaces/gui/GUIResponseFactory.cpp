ac351ca0cd8450d3954a90fe75498812f02658d8
HBuczynski <hubert.buczynski94@gmail.com>
2018-11-01 15:36:46 +0100

    Added new command to response factory.

--- src/interfaces/gui/GUIResponseFactory.cpp
+++ src/interfaces/gui/GUIResponseFactory.cpp
@@ -1,6 +1,7 @@
 #include "GUIResponseFactory.h"
 
 #include "GUIPlaneBuilder.h"
+#include "GUIWindowResponseBuilder.h
 
 using namespace std;
 using namespace communication;
@@ -19,6 +20,9 @@ unique_ptr<GUIResponse> GUIResponseFactory::createCommand(const vector<uint8_t> 
     {
         case GUIResponseType::SET_PLANE :
             builder_ = make_unique<GUIPlaneBuilder>();
+            return move(builder_->create(commandInBytes));
+        case GUIResponseType::WINDOWS_TYPE :
+            builder_ = make_unique<GUIWindowResponseBuilder>();
             return move(builder_->create(commandInBytes));
         default:
             throw invalid_argument("Received command does not register in factory.");
