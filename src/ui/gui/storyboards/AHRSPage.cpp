d838da43f9bd2283fa94797bc898da71bba20f46
HBuczynski <hubert.buczynski94@gmail.com>
2018-11-02 20:10:43 +0100

    Updated measuremenets.

--- src/ui/gui/storyboards/AHRSPage.cpp
+++ src/ui/gui/storyboards/AHRSPage.cpp
@@ -276,4 +276,6 @@ void AHRSPage::handleFlightDataCommand(const FlightMeasurements& measurements)
     setPressure(measurements.pressure);
     setClimbRate(measurements.verticalSpeed);
     setMachNo(measurements.machNo);
+
+    ui_->setupUi(this);
 }
