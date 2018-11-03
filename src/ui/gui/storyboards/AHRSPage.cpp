bd7e17b0e309a8bcd41745bbf259c71427bb78a4
HBuczynski <hubert.buczynski94@gmail.com>
2018-11-03 16:23:07 +0100

    Added ne timer.

--- src/ui/gui/storyboards/AHRSPage.cpp
+++ src/ui/gui/storyboards/AHRSPage.cpp
@@ -29,7 +29,7 @@ AHRSPage::AHRSPage(gui::PageController *controller, QWidget *parent)
 
     connect(&acqTimer_, SIGNAL(timeout()), this, SLOT(acquireFlightData()));
     connect(this, SIGNAL(signalStopTimer()), this, SLOT(stopTimer()));
-    acqTimer_.start(200);
+    acqTimer_.start(100);
 }
 
 AHRSPage::~AHRSPage()
