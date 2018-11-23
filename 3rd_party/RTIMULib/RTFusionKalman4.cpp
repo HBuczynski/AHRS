0ba39d87c335ec6a48cfa9f73c5663446802e8c8
HBuczynski <hubert.buczynski94@gmail.com>
2018-11-23 18:52:18 +0100

    Updated kalman.

--- 3rd_party/RTIMULib/RTFusionKalman4.cpp
+++ 3rd_party/RTIMULib/RTFusionKalman4.cpp
@@ -192,6 +192,7 @@ void RTFusionKalman4::newIMUData(RTIMU_DATA& data, const RTIMUSettings *settings
         m_lastFusionTime = data.timestamp;
 
         calculatePose(m_accel, m_compass, settings->m_compassAdjDeclination);
+        calculatePose(getGravity(), data.compass, settings->m_compassAdjDeclination);
 
         m_Fk.fill(0);
 
@@ -223,6 +224,7 @@ void RTFusionKalman4::newIMUData(RTIMU_DATA& data, const RTIMUSettings *settings
         }
 
         calculatePose(data.accel, data.compass, settings->m_compassAdjDeclination);
+        calculatePose(getGravity(), data.compass, settings->m_compassAdjDeclination);
 
         predict();
         update();
--- src/telemetry/PlaneOrientation.cpp
+++ src/telemetry/PlaneOrientation.cpp
@@ -12,11 +12,6 @@ PlaneOrientation::PlaneOrientation()
 {
     settings = new RTIMUSettings("RTIMULib");
     imu = RTIMU::createIMU(settings);
-
-    m_gravity.setScalar(0);
-    m_gravity.setX(0);
-    m_gravity.setY(0);
-    m_gravity.setZ(1);
 }
 
 PlaneOrientation::~PlaneOrientation()
@@ -44,10 +39,6 @@ bool PlaneOrientation::initDataAcquisition()
     imu->setAccelEnable(true);
     imu->setCompassEnable(true);
 
-    prevX = 0;
-    prevY = 0;
-    prevZ = 0;
-
     return true;
 }
 
@@ -58,36 +49,21 @@ void PlaneOrientation::readData()
     while (imu->IMURead())
     {
         imuData = imu->getIMUData();
-
-        fusedConjugate = imuData.fusionQPose.conjugate();
-
-        // now do the rotation - takes two steps with qTemp as the intermediate variable
-
-        qTemp = m_gravity * imuData.fusionQPose;
-        rotatedGravity = fusedConjugate * qTemp;
-
-        residuals.setX((rotatedGravity.x()));
-        residuals.setY((rotatedGravity.y()));
-        residuals.setZ((rotatedGravity.z()));
-
     }
 }
 
 float PlaneOrientation::getPitch()
 {
-    prevY = prevY *0.8 + residuals.y()*0.2*RTMATH_RAD_TO_DEGREE;
-    return prevY;
+    return imuData.fusionPose.y()*RTMATH_RAD_TO_DEGREE;
 }
 
 float PlaneOrientation::getRoll()
 {
-    prevX = prevX *0.8 + residuals.x()*0.2*RTMATH_RAD_TO_DEGREE;
-    return prevX;
+    return imuData.fusionPose.x()*RTMATH_RAD_TO_DEGREE;
 }
 
 float PlaneOrientation::getYaw()
 {
-    prevZ = prevZ *0.8 + residuals.z()*0.2*RTMATH_RAD_TO_DEGREE;
-    return prevZ;
+    return imuData.fusionPose.z()*RTMATH_RAD_TO_DEGREE;
 }
 
